1c213091277033373511fcac855a17e20b65fc38
MohammadRokib <rokibk548@gmail.com>
2022-07-25 11:51:35 +0600

    -m "completed cses_Appartments"

--- cses_fi/cses_Appartments.cpp
+++ cses_fi/cses_Appartments.cpp
@@ -13,5 +13,25 @@ int main()
     for (int i = 0; i < n; i++)
         cin >> ara[i];
     for (int i = 0; i < m; i++)
-        cin >> arr[m];
+        cin >> arr[i];
+    
+    sort (ara, ara+n);
+    sort (arr, arr+m);
+
+    int j = 0;
+    int i = 0;
+    int ans = 0;
+    while (i < n && j < m) {
+        int x = (ara[i]-arr[j]);
+
+        if (x >= -k && x <= k) {
+            i++; j++;
+            ans++;
+        }
+        else if (x < -k) i++;
+        else j++;
+            
+    }
+    cout << ans << endl;
+    return 0;
 }
